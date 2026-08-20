474227089007ba479c2cb08f2b7428d52c0f6864
Khairul Haiqal <malikibambam09@gmail.com>
2026-08-21 02:36:47 +0800

    feat: add 7 new investigation cases (8 total)
    - CASE-002: The Vanishing Heiress (missing person, medium)
    - CASE-003: The Midtown Arson (arson, hard)
    - CASE-004: The Corporate Poison (murder, medium)
    - CASE-005: The Museum Heist (theft, hard)
    - CASE-006: The Dinner Party Death (murder, expert)
    - CASE-007: The Lighthouse Keeper (murder, medium)
    - CASE-008: The Digital Ghost (cybercrime, hard)

--- src/cases/index.ts
+++ src/cases/index.ts
@@ -1,7 +1,23 @@
 import { Case } from "@/types";
 import { lockedRoomCase } from "./locked-room";
+import { vanishingHeiressCase } from "./vanishing-heiress";
+import { midtownArsonCase } from "./midtown-arson";
+import { corporatePoisonCase } from "./corporate-poison";
+import { museumHeistCase } from "./museum-heist";
+import { dinnerPartyDeathCase } from "./dinner-party-death";
+import { lighthouseKeeperCase } from "./lighthouse-keeper";
+import { digitalGhostCase } from "./digital-ghost";
 
-export const cases: Case[] = [lockedRoomCase];
+export const cases: Case[] = [
+  lockedRoomCase,
+  vanishingHeiressCase,
+  midtownArsonCase,
+  corporatePoisonCase,
+  museumHeistCase,
+  dinnerPartyDeathCase,
+  lighthouseKeeperCase,
+  digitalGhostCase,
+];
 
 export function getCaseById(id: string): Case | undefined {
   return cases.find((c) => c.metadata.id === id);
